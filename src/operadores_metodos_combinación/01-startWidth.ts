b80abf25f54c6fac0f073073d15baf2751bb52fc
Alejandro González Reyes <alexglzrys@gmail.com>
2022-05-15 10:59:48 -0500

    operador startWidth

---
+++ src/operadores_metodos_combinación/01-startWidth.ts
@@ -0,0 +1,15 @@
+import { of } from 'rxjs';
+import { startWith } from 'rxjs/operators'
+
+/**
+ * Operador startWidth
+ * Empieza con el argumento que se le mande (envía una primera emisión), y posterior a ello combina el contenido
+ * que viaja en la emisión del Observable original
+ */
+
+const numeros$ = of(1,2,3);
+
+numeros$.pipe(
+    // En este caso la emisión sería: hola 1 2 3
+    startWith('hola')
+).subscribe(console.log)
